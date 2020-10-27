5b23ba88fb0ee60bb1ab89eb38d8b24d507289f2
Gasa91 <yoanysanchez1212@gmail.com>
2020-10-26 21:29:41 -0500

    commit

--- application/controllers/Update_C.php
+++ application/controllers/Update_C.php
@@ -38,29 +38,3 @@ class Update_C extends CI_Controller{
        
     
 }
-
-    // public function update($identification)
-	// {
-    //     // $typeIdentification=$this->input->post('typeIdentification');
-    //     $identification=$this->input->post('identification');
-	//     $result=$this->Update_M->update($identification );
-	//     $this->load->view('update_V',$result);	
-	
-	// 	if($this->input->post('update')){
-	// 	$typeIdentification=$this->input->post('typeIdentification');
-    //     $user=$this->input->post('user');
-    //     $passw=$this->input->post('passw');
-    //     $name=$this->input->post('name');
-    //     $lastname=$this->input->post('lastname');
-    //     $email=$this->input->post('email');
-
-
-	// 	$this->Update_M->update($user,$passw,$name,$lastname,$email,$identification,$typeIdentification);
-	// 	$this->load->view('Layouts/header');
-	// 	$this->load->view('Update_V');
-    //     $this->load->view('Layouts/footer');
-	// 	}
-    
-
-    // }
-// }
